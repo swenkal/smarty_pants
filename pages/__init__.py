440af93a02f2a7787faf9e2537436a58a4b17a3f
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-05-25 22:51:52 +0300

    Pages: added params validate for get methods and polishing

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,67 +1,125 @@
-from flask_restful import Resource
-from clickhouse_driver import connect
 from datetime import datetime
-from marshmallow import ValidationError
-from .schema import PageSchema
-from flask import request
 import json
+
+from flask import request, make_response, jsonify
+from flask_restful import Resource
+from marshmallow import ValidationError
+from clickhouse_driver import connect
+
+from .schema import PageSchema, GetSchema
+
 
 conn = connect('clickhouse://localhost')
 page_cursor = conn.cursor()
 select_cursor = conn.cursor()
 
+
 class Pages(Resource):
     def get(self):
         #get parameters
-        req_info = request.args.to_dict();
-        print(req_info['type'])
-        #TODO check for get and post functions!!!
-        #checking if a method exists
-        if hasattr(self, req_info['type']) and callable(getattr(self, req_info['type'])):
-            func = getattr(self, req_info['type'])
+        req_info = request.args.to_dict()
+
+        errors = GetSchema().validate(req_info)
+        if errors:
+            err_response = make_response(errors, 400)
+            self.set_access_headers(err_response)
+            return err_response
+
+        type = req_info['type']
+        #check for get and post and builtIn functions
+        if type == 'get' or type == 'post':
+            err_response = make_response(
+                {'message': 'Not supported method'},
+                400
+            )
+            self.set_access_headers(err_response)
+            return err_response
+
+        #checking if a method exists - call it
+        if hasattr(self, type) and callable(getattr(self, type)):
+            func = getattr(self, type)
             return func(req_info)
-        else:
-            return {'message': 'Not found'}, 404
+
+        err_response = make_response({'message': 'Not found'}, 404)
+        self.set_access_headers(err_response)
+        return err_response
+
     def common_info(self, params):
         #TODO: check input params
         #TODO: errors processing
-        select_cursor.execute('SELECT  count() AS visits, '
-                'countIf(activeTime < 5) AS bouncedVisits, '
-                '(100. * bouncedVisits) / visits AS bounceRate '
-                'FROM    smartyPants.pages '
-                'WHERE   hitDate BETWEEN %(start)s  AND %(end)s  AND '
-                         'pathName = %(pathName)s',
-                #{'start': '2021-05-09', 'end': '2021-05-12', 'disc': '/disciplines/the-newest/'})
-                {'start': params['start'], 'end': params['end'], 'pathName': params['pathName']})
+        if 'pathName' not in params:
+            err_response = make_response(
+                {'message': 'Not enough parameters'},
+                400
+            )
+            self.set_access_headers(err_response)
+            return err_response
+
+        select_cursor.execute(
+            'SELECT  uniq(uniqueID) AS unique, count() AS visits, '
+            'countIf(activeTime < 5) AS bouncedVisits, '
+            '(100. * bouncedVisits) / visits AS bounceRate '
+            'FROM    smartyPants.pages '
+            'WHERE   hitDate BETWEEN %(start)s  AND %(end)s  AND '
+                     'pathName = %(pathName)s',
+            {
+                'start': params['start'],
+                'end': params['end'],
+                'pathName': params['pathName']
+            }
+        )
+
         dbResult = select_cursor.fetchone()
-        response = {'visits': dbResult[0], 'bouncedVisits': dbResult[1], 'bounceRate': dbResult[2]}
-        return response, 200
+        response = make_response(
+            jsonify(
+                {
+                    'unique': dbResult[0],
+                    'visits': dbResult[1],
+                    'bouncedVisits': dbResult[2],
+                    'bounceRate': round(dbResult[3], 2)
+                }
+            ),
+            200
+        )
+        self.set_access_headers(response)
+        return response
+
+    def set_access_headers(self, response):
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "*")
+
     def post(self):
         page_info = json.loads(request.data)
+
         try:
-            print(page_info['referrer'])
             page_info = PageSchema().load(page_info)
-            #в зависимости от того, как отправляется дата в ClickHouse
-            #hitDate = page_info['hitDate']
-            #page_info['hitDate'] = aniso8601.parse_datetime(hitDate)
-
-            #get seconds for activeTime
-            page_info['activeTime'] = page_info['activeTime'] // 1000
-            page_info['hitDate'] = page_info['hitDate'] // 1000
-            print(page_info)
-
-            page_cursor.executemany('INSERT INTO smartyPants.pages VALUES',
-                              [[
-                                page_info['uniqueID'], page_info['visitID'],
-                                page_info['pathName'], page_info['loadTime'],
-                                page_info['activeTime'], page_info['hitDate'],
-                                datetime.fromtimestamp(page_info['hitDate']),
-                                page_info['referrer']
-                              ]])
-
-            with open ('./pages/data.txt', 'a+') as file:
-                file.write(json.dumps(page_info)+'\n')
-            file.close()
         except ValidationError as err:
             print(err.messages)
-        return {} , 200
+            err_response = make_response(err.messages, 400)
+            self.set_access_headers(err_response)
+            return err_response
+
+        #get seconds for activeTime
+        page_info['activeTime'] = page_info['activeTime'] // 1000
+        page_info['hitDate'] = page_info['hitDate'] // 1000
+        print(page_info)
+
+        page_cursor.executemany(
+            'INSERT INTO smartyPants.pages VALUES',
+            [[
+                page_info['uniqueID'], page_info['visitID'],
+                page_info['pathName'], page_info['loadTime'],
+                page_info['activeTime'], page_info['hitDate'],
+                datetime.fromtimestamp(page_info['hitDate']),
+                page_info['referrer']
+            ]]
+        )
+
+        with open ('./pages/data.txt', 'a+') as file:
+            file.write(json.dumps(page_info)+'\n')
+            file.close()
+
+        response = make_response({'message': 'ok'}, 200)
+        response.set_access_headers(response)
+        return response
--- pages/schema.py
+++ pages/schema.py
@@ -1,4 +1,5 @@
 from marshmallow import Schema, fields
+from marshmallow.validate import Regexp
 
 class PageSchema(Schema):
     uniqueID = fields.Int(required=True)
@@ -10,3 +11,14 @@ class PageSchema(Schema):
     hitDate = fields.Int(required=True)
     #host = fields.Str(required=True)
     #hitDate = fields.Str(required=True, validate=Regexp(zulu_regexp))
+
+#regexp for builtIn methods like __magic__
+type_regexp = r"^(?:(?!__.*__).)*$"
+
+date_regexp = r"^20\d{2}-[0-1]\d-[0-3]\d$"
+
+class GetSchema(Schema):
+    type = fields.Str(required=True, validate=Regexp(type_regexp))
+    start = fields.Str(required=True, validate=Regexp(date_regexp))
+    end = fields.Str(required=True, validate=Regexp(date_regexp))
+    pathName = fields.Str()
