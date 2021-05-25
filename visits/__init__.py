e282838d7a4e2d93e475f0a0ccb3d0b6a648e95d
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-05-25 22:52:42 +0300

    Visits: Added polishing

--- visits/__init__.py
+++ visits/__init__.py
@@ -1,11 +1,12 @@
-from flask_restful import Resource
-from clickhouse_driver import connect
 from datetime import datetime
-from marshmallow import ValidationError
-from .schema import VisitSchema
-from flask import request
 import json
 
+from flask import request
+from flask_restful import Resource
+from marshmallow import ValidationError
+from clickhouse_driver import connect
+
+from .schema import VisitSchema
 
 conn = connect('clickhouse://localhost')
 visit_cursor = conn.cursor()
@@ -15,30 +16,32 @@ class Visits(Resource):
         visit_info = json.loads(request.data)
         try:
             visit_info = VisitSchema().load(visit_info)
-
-            #преобразование bool -> int
-            cookieAgree = visit_info['cookieAgree']
-            visit_info['cookieAgree'] = int(cookieAgree == True)
-
-            #получение индекса браузера в листе для сохранения в базе
-            browser = visit_info['browser']
-            visit_info['browser'] = browsersList.index(browser)
-
-            visit_info['createDate'] = visit_info['createDate'] // 1000
-            print(visit_info)
-            visit_cursor.executemany('INSERT INTO smartyPants.visits VALUES',
-                                [[
-                                visit_info['uniqueID'], visit_info['visitID'],
-                                visit_info['screenHeight'], visit_info['screenWidth'],
-                                visit_info['cookieAgree'], visit_info['browser'],
-                                visit_info['createDate'],
-                                datetime.fromtimestamp(visit_info['createDate'])
-                                ]])
-            with open ('./visits/data.txt', 'a+') as file:
-                file.write(json.dumps(visit_info)+'\n')
-                file.close()
         except ValidationError as err:
             print(err.messages)
+            return err.messages, 400
+
+        #преобразование bool -> int
+        cookieAgree = visit_info['cookieAgree']
+        visit_info['cookieAgree'] = int(cookieAgree == True)
+
+        #получение индекса браузера в листе для сохранения в базе
+        browser = visit_info['browser']
+        visit_info['browser'] = browsersList.index(browser)
+
+        visit_info['createDate'] = visit_info['createDate'] // 1000
+        print(visit_info)
+        visit_cursor.executemany('INSERT INTO smartyPants.visits VALUES',
+                    [[
+                        visit_info['uniqueID'], visit_info['visitID'],
+                        visit_info['screenHeight'], visit_info['screenWidth'],
+                        visit_info['cookieAgree'], visit_info['browser'],
+                        visit_info['createDate'],
+                        datetime.fromtimestamp(visit_info['createDate'])
+                    ]])
+        with open ('./visits/data.txt', 'a+') as file:
+            file.write(json.dumps(visit_info)+'\n')
+            file.close()
+
         return {} , 200
 
 browsersList = [
