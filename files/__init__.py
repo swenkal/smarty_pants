c41891778920e42604c1deb1ea48e3880f664681
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-05-25 22:51:05 +0300

    Added params validate for get methods and polishing

--- files/__init__.py
+++ files/__init__.py
@@ -1,95 +1,186 @@
-from flask_restful import Resource
-from clickhouse_driver import connect
 from datetime import datetime
-from marshmallow import ValidationError
-from .schema import FileSchema
-from flask import request
 import json
+
+from flask import request, make_response, jsonify
+from flask_restful import Resource
+from marshmallow import ValidationError
+from clickhouse_driver import connect
+
+from .schema import FileSchema, GetSchema
 
 conn = connect('clickhouse://localhost')
 file_cursor = conn.cursor()
 select_cursor = conn.cursor()
+
 class Files(Resource):
     def get(self):
         req_info = request.args.to_dict();
-        print(req_info['type'])
-        #TODO check for get and post functions!!!
+
+        errors = GetSchema().validate(req_info)
+        if errors:
+            err_response = make_response(errors, 400)
+            self.set_access_headers(err_response)
+            return err_response
+
+        type = req_info['type']
+        if type == 'get' or type == 'post':
+            err_response = make_response(
+                {'message': 'Not supported method'},
+                400
+            )
+            self.set_access_headers(err_response)
+            return err_response
+
         #checking if a method exists
-        if hasattr(self, req_info['type']) and callable(getattr(self, req_info['type'])):
-            func = getattr(self, req_info['type'])
+        if hasattr(self, type) and callable(getattr(self, type)):
+            func = getattr(self, type)
             return func(req_info)
-        else:
-            return {'message': 'Not found'}, 404
+
+        err_response = make_response({'message': 'Not found'}, 404)
+        self.set_access_headers(err_response)
+        return err_response
+
     def common_info(self, params):
-        #TODO: check input params
-        #TODO: errors processing
-        select_cursor.execute('SELECT count() AS downloadedFiles '
-                'FROM smartyPants.files '
-                'WHERE loadDate BETWEEN %(start)s  AND %(end)s  AND '
-                        'discipline = %(disc)s',
-                {'start': params['start'],
-                 'end': params['end'],
-                 'disc': params['disc']
-                 })
+
+        #check additional param
+        if 'disc' not in paramss:
+            err_response = make_response(
+                {'message': 'Not enough parameters'},
+                400
+            )
+            self.set_access_headers(err_response)
+            return err_response
+
+        select_cursor.execute(
+            'SELECT count() AS downloadedFiles '
+            'FROM smartyPants.files '
+            'WHERE loadDate BETWEEN %(start)s  AND %(end)s  AND '
+                    'discipline = %(disc)s',
+            {
+                'start': params['start'],
+                'end': params['end'],
+                'disc': params['disc']
+            }
+        )
         dbResult = select_cursor.fetchone()
-        return { 'downloadedFiles': dbResult[0] }, 200
+
+        response = make_response(
+            jsonify(
+                {
+                    'downloads': dbResult[0]
+                }
+            ),
+            200
+        )
+        self.set_access_headers(response)
+        return response
+
     def disc_info(self, params):
-        #TODO: check input params
-        #TODO: errors processing
-        select_cursor.execute('SELECT toString(loadDate), count() AS downloads '
-                'FROM smartyPants.files '
-                'WHERE loadDate BETWEEN %(start)s AND %(end)s AND '
-                'discipline = %(disc)s'
-                'GROUP BY loadDate',
-                {'start': params['start'],
-                 'end': params['end'],
-                 'disc': params['disc']
-                 })
-        response = {'graph':{}}
+
+        #check additional param
+        if 'disc' not in paramss:
+            err_response = make_response(
+                {'message': 'Not enough parameters'},
+                400
+            )
+            self.set_access_headers(err_response)
+            return err_response
+
+        select_cursor.execute(
+            'SELECT loadDate, count() AS downloads '
+            'FROM smartyPants.files '
+            'WHERE loadDate BETWEEN %(start)s AND %(end)s AND '
+                   'discipline = %(disc)s '
+            'GROUP BY loadDate '
+            'ORDER BY loadDate ASC '
+            'WITH FILL FROM toDate(%(start)s) TO toDate(%(end)s) STEP 1',
+            {
+                'start': params['start'],
+                'end': params['end'],
+                'disc': params['disc']
+            }
+        )
+
+        result = {'graph':{}}
         #TODO change logic for fetchall
         while (row := select_cursor.fetchone()):
-            response['graph'][row[0]] = row[1]
-        
-        select_cursor.execute('SELECT fileID, count() AS downloads, '
-                'countIf(isArchive = 1) AS archiveCount '
-                'FROM smartyPants.files '
-                'WHERE loadDate BETWEEN %(start)s AND %(end)s AND '
-                'discipline = %(disc)s'
-                'GROUP BY fileID '
-                'ORDER BY downloads DESC',
-                {'start': params['start'],
-                 'end': params['end'],
-                 'disc': params['disc']
-                 })
-        response['top'] = []
+            dateStr = row[0].strftime("%Y-%m-%d")
+            result['graph'][dateStr] = row[1]
+
+        select_cursor.execute(
+            'SELECT fileID, count() AS downloads, '
+            'countIf(isArchive = 1) AS archiveCount '
+            'FROM smartyPants.files '
+            'WHERE loadDate BETWEEN %(start)s AND %(end)s AND '
+                   'discipline = %(disc)s'
+            'GROUP BY fileID '
+            'ORDER BY downloads DESC',
+            {
+                'start': params['start'],
+                'end': params['end'],
+                'disc': params['disc']
+            }
+        )
+        result['top'] = []
         #TODO change logic for fetchall
         while (row := select_cursor.fetchone()):
-            file = {'fileID': row[0], 'downloads': row[1], 'archive': row[2]}
-            response['top'].append(file)
-        return response, 200
+            file = {
+                'fileID': row[0],
+                'downloads': row[1],
+                'archive': row[2]
+            }
+            result['top'].append(file)
+
+        response = make_response(jsonify(result),200)
+        self.set_access_headers(response)
+        return response
+
     def file_info(self, params):
         #TODO: check input params
         #TODO: errors processing
-        select_cursor.execute('SELECT userGroup, uniqueID, count() AS downloads '
-                'FROM smartyPants.files '
-                'WHERE loadDate BETWEEN %(start)s AND %(end)s AND '
-                'fileID = %(fileID)s '
-                'GROUP BY userGroup, uniqueID WITH ROLLUP',
-                {'start': params['start'],
-                 'end': params['end'],
-                 'fileID': params['fileID']
-                 })
-        dbResult = select_cursor.fetchall()
-        response = { 'detailed': [], 'total': [] }
+        if 'fileID' not in params:
+            err_response = make_response(
+                {'message': 'Not enough parameters'},
+                400
+            )
+            self.set_access_headers(err_response)
+            return err_response
 
+        select_cursor.execute(
+            'SELECT userGroup, uniqueID, count() AS downloads '
+            'FROM smartyPants.files '
+            'WHERE loadDate BETWEEN %(start)s AND %(end)s AND '
+                   'fileID = %(fileID)s '
+            'GROUP BY userGroup, uniqueID WITH ROLLUP',
+            {
+                'start': params['start'],
+                'end': params['end'],
+                'fileID': params['fileID']
+            }
+        )
+        dbResult = select_cursor.fetchall()
+
+        result = { 'detailed': [], 'total': [] }
         for row in dbResult:
         #while (row := select_cursor.fetchone()):
-            rowDict = { 'group': row[0], 'ID': row[1], 'count': row[2] }
+            rowDict = {
+                'group': row[0],
+                'ID': row[1],
+                'count': row[2]
+            }
             if rowDict['ID'] == 0:
-                response['total'].append(rowDict)
+                result['total'].append(rowDict)
             else:
-                response['detailed'].append(rowDict)
-        return response, 200
+                result['detailed'].append(rowDict)
+
+        response = make_response(jsonify(result), 200)
+        self.set_access_headers(response)
+        return response
+
+    def set_access_headers(self, response):
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "*")
 
     def post(self):
         files_info = json.loads(request.data)
@@ -100,16 +191,27 @@ class Files(Resource):
             files_list.append(files_info)
         try:
             files_list = FileSchema(many=True).load(files_list)
-            files_output = list(map(prepareFilesInfo, files_list))
-            print(files_list)
-            file_cursor.executemany('INSERT INTO smartyPants.files VALUES',
-                                    files_output)
-            with open ('./files/data.txt', 'a+') as file:
-                file.write(json.dumps(files_list)+'\n')
-            file.close()
         except ValidationError as err:
             print(err.messages)
-        return {} , 200
+            err_response = make_response(err.messages, 400)
+            self.set_access_headers(err_response)
+            return err_response
+
+        files_output = list(map(prepareFilesInfo, files_list))
+        print(files_list)
+
+        file_cursor.executemany(
+            'INSERT INTO smartyPants.files VALUES',
+            files_output
+        )
+
+        with open ('./files/data.txt', 'a+') as file:
+            file.write(json.dumps(files_list)+'\n')
+            file.close()
+
+        response = make_response({'message': 'ok'}, 200)
+        response.set_access_headers(response)
+        return response
 
 def prepareFilesInfo(dict):
     dict['isArchive'] = int(dict['isArchive'] == True)
--- files/schema.py
+++ files/schema.py
@@ -11,3 +11,15 @@ class FileSchema(Schema):
     referrer = fields.Str(required=True) #url не подходит в связи с тем, что может приходить пустота
     downloadDate = fields.Int(required=True)
     #downloadDate = fields.Str(required=True, validate=Regexp(zulu_regexp))
+
+#regexp for builtIn methods like __magic__
+type_regexp = r"^(?:(?!__.*__).)*$"
+
+date_regexp = r"^20\d{2}-[0-1]\d-[0-3]\d$"
+
+class GetSchema(Schema):
+    type = fields.Str(required=True, validate=Regexp(type_regexp))
+    start = fields.Str(required=True, validate=Regexp(date_regexp))
+    end = fields.Str(required=True, validate=Regexp(date_regexp))
+    fileID = fields.Str()
+    disc = fields.Str()
